60e2220f8ec8bfc89b21ef49f74bfceced77e4cd
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-06 16:09:00 -0500

    lint

--- test.js
+++ test.js
@@ -7,26 +7,26 @@
 
 'use strict';
 
-/* deps:mocha */
+require('mocha');
+require('should');
 var fs = require('fs');
-var should = require('should');
 var handlebars = require('handlebars');
 var coverage = require('./');
 
 var expected = fs.readFileSync('fixtures/expected.txt', 'utf8');
 
-describe('coverage', function () {
-  it('should include a formatted coverage report:', function () {
+describe('coverage', function() {
+  it('should include a formatted coverage report:', function() {
     coverage('fixtures/summary.txt').should.equal(expected);
   });
 
-  it('should work as a handlebars helper:', function () {
+  it('should work as a handlebars helper:', function() {
     handlebars.registerHelper('coverage', coverage);
     handlebars.compile('{{coverage "fixtures/summary.txt"}}')().should.equal(expected);
   });
 
-  it('should throw an error when the file cannot be found:', function () {
-    (function () {
+  it('should throw an error when the file cannot be found:', function() {
+    (function() {
       coverage('foo');
     }).should.throw('helper-coverage cannot find: foo');
   });
